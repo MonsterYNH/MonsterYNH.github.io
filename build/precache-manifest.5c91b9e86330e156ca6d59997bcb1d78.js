abcb93a996a2dda2d9ea84efb28b06cdc4b71f61
monster <ynhmonster@163.com>
2019-05-10 23:10:25 +0800

    update

---
+++ build/precache-manifest.5c91b9e86330e156ca6d59997bcb1d78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "16f3d03fe77269735837b19d1089ba5d",
+    "url": "./static/media/wechat.16f3d03f.jpg"
+  },
+  {
+    "revision": "8340ff3f31d850668b70832ed4cc1b15",
+    "url": "./static/media/qq.8340ff3f.jpg"
+  },
+  {
+    "revision": "6e99af6760012e5ce5b9a75c1c2b6e4e",
+    "url": "./static/media/monsterlogo.6e99af67.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "3619628163774a32adf5",
+    "url": "./static/js/main.36196281.chunk.js"
+  },
+  {
+    "revision": "c2b53b51da28bdc1e637",
+    "url": "./static/js/2.c2b53b51.chunk.js"
+  },
+  {
+    "revision": "3619628163774a32adf5",
+    "url": "./static/css/main.dea9a563.chunk.css"
+  },
+  {
+    "revision": "665ce223314e67e37800ef362b13efa0",
+    "url": "./index.html"
+  }
+];
